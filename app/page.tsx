9c6925db2ec46bb821ff7cd76ea8dd6503db394c
Vidyut Venkatesan <73650180+Vidyut01@users.noreply.github.com>
2024-06-22 09:51:36 +0000

    Added framer motion animations

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,8 @@
+'use client' // TODO: DON'T USE CLIENT HERE
+
 import Image from "next/image";
 import { Reem_Kufi } from "next/font/google";
+import { motion } from "framer-motion";
 
 const subtitleFont = Reem_Kufi({
   weight: '400',
@@ -24,17 +27,29 @@ export default function Home() {
       </header>
 
       <hr className="mx-28 rounded-full h-1 bg-gray-600 my-10" />
-
+      
       <main className="px-32 mb-10">
-        <section className="flex justify-between">
-          <article className="w-[75%]">
+        <section className="flex justify-between overflow-hidden">
+          <motion.article
+            className="w-[75%] flex flex-col items-center"
+            initial={{ opacity: '0', x: '-100%' }}
+            whileInView={{ opacity: '1', x: '0' }}
+            viewport={{ once: true }}
+            transition={{ duration: .75 }}
+          >
             <h2 className={`text-[3rem] text-center ${subtitleFont.className}`}>Who am I ?</h2>
             <br/>
-            <p className="text-xl">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Recusandae beatae iusto eligendi odit dolore aut quasi consequuntur reiciendis, at hic incidunt odio cumque assumenda iste fuga quibusdam a voluptatibus in?</p>
-          </article>
-          <figure className="w-auto">
+            <p className="text-xl max-w-[80%]">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Recusandae beatae iusto eligendi odit dolore aut quasi consequuntur reiciendis, at hic incidunt odio cumque assumenda iste fuga quibusdam a voluptatibus in?</p>
+          </motion.article>
+          <motion.figure 
+            className="w-auto"
+            initial={{ opacity: '0', x: '100%' }}
+            whileInView={{ opacity: '1', x: '0' }}
+            viewport={{ once: true }}
+            transition={{ duration: .75 }}
+          >
             <Image src="/portrait.jpeg" alt="profile_image" className="rounded-3xl" width={400} height={400} />
-          </figure>
+          </motion.figure>
         </section>
       </main>
     </div>
