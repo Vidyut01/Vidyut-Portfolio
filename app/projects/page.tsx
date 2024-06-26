dd9674df98d427d0fe1268d17e81e3bf30faa32b
Vidyut Venkatesan <vidyut0903@gmail.com>
2024-06-26 20:35:14 +1000

    added title to project page

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -7,34 +7,45 @@ import Image from 'next/image'
 const Projects = () => {
   return (
     <div className='mt-16 mx-36 mb-10'>
-      <LeftSidePopUp>
-        <LeftSidePopUp.Title>
-          Project 1
-        </LeftSidePopUp.Title>
+      <header className='h-screen flex items-center justify-evenly'>
+        <div className='text-center'>
+          <h1 className='text-4xl mb-10'>Projects</h1>
+          <h3 className='text-2xl'>Here are some of the projects I&#39;ve worked on</h3>
+        </div>
+        <figure>
+          <img src='https://picsum.photos/300' />
+        </figure>
+      </header>
 
-        <LeftSidePopUp.Content>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure non provident, rem ratione quisquam sint architecto cum. Ducimus porro sequi debitis architecto dolorum laboriosam id soluta, eveniet possimus ipsa quia.
-        </LeftSidePopUp.Content>
+      <main>
+        <LeftSidePopUp>
+          <LeftSidePopUp.Title>
+            Project 1
+          </LeftSidePopUp.Title>
 
-        <LeftSidePopUp.Image>
-          <Image src={'/next.svg'} alt="pic" width={350} height={350} />
-        </LeftSidePopUp.Image>
-      </LeftSidePopUp>
+          <LeftSidePopUp.Content>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure non provident, rem ratione quisquam sint architecto cum. Ducimus porro sequi debitis architecto dolorum laboriosam id soluta, eveniet possimus ipsa quia.
+          </LeftSidePopUp.Content>
 
-      <RightSidePopUp>
-        <RightSidePopUp.Title>
-          Project 2
-        </RightSidePopUp.Title>
+          <LeftSidePopUp.Image>
+            <Image src={'/next.svg'} alt="pic" width={350} height={350} />
+          </LeftSidePopUp.Image>
+        </LeftSidePopUp>
 
-        <RightSidePopUp.Content>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure non provident, rem ratione quisquam sint architecto cum. Ducimus porro sequi debitis architecto dolorum laboriosam id soluta, eveniet possimus ipsa quia.
-        </RightSidePopUp.Content>
-        
-        <RightSidePopUp.Image>
-          <Image src={'/next.svg'} alt="pic" width={350} height={350} />
-        </RightSidePopUp.Image>
-      </RightSidePopUp>
-      
+        <RightSidePopUp>
+          <RightSidePopUp.Title>
+            Project 2
+          </RightSidePopUp.Title>
+
+          <RightSidePopUp.Content>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure non provident, rem ratione quisquam sint architecto cum. Ducimus porro sequi debitis architecto dolorum laboriosam id soluta, eveniet possimus ipsa quia.
+          </RightSidePopUp.Content>
+          
+          <RightSidePopUp.Image>
+            <Image src={'/next.svg'} alt="pic" width={350} height={350} />
+          </RightSidePopUp.Image>
+        </RightSidePopUp>
+      </main>
     </div>
   )
 }
