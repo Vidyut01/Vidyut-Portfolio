e5e4b7a3dea7f7c337ddb8cf16255a7b5de9d092
Vidyut Venkatesan <73650180+Vidyut01@users.noreply.github.com>
2024-08-05 07:50:33 +0000

    Update github url in footer

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
         <h4 className="text-center px-5">Made by Vidyut &copy; 2024</h4>
         <h4 className="text-center px-5">
           The Source Code for this site can be found on {""}
-          <a className="text-blue-400 hover:underline" href="https://github.com/Vidyut01/portfolio">
+          <a className="text-blue-400 hover:underline" href="https://github.com/Vidyut01/Vidyut-Portfolio">
             GitHub
           </a>
         </h4>
