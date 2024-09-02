e9c81501f297e5a464bac2b67668bb4f409eaed1
Vidyut Venkatesan <vidyut0903@gmail.com>
2024-09-02 14:10:28 +0530

    Seperated sections into components

--- components/Particles/index.tsx
+++ components/Particles/index.tsx
@@ -1,101 +1,22 @@
 "use client";
 
-import React, { useCallback } from "react";
+import React, { use, useCallback } from "react";
 import Particles from "react-tsparticles";
 import { loadFull } from "tsparticles";
+import { Engine } from "tsparticles-engine";
 
 const BGParticles = () => {
-  const particlesInit = useCallback(async (engine: any) => {
+  const particlesInit = useCallback(async (engine: Engine) => {
     await loadFull(engine);
   }, []);
 
-  const particlesLoaded = useCallback(async (container: any) => {
-    await console.log(container);
-  }, []);
-
-  const options: any = {
-    background: {
-      color: "#121212",
-    },
-    interactivity: {
-      events: {
-        onClick: {
-          enable: true,
-          mode: "explode"
-        },
-        onHover: {
-          enable: true,
-          mode: "repulse",
-        },
-        resize: true,
-      },
-      modes: {
-        bubble: {
-          distance: 400,
-          duration: 2,
-          opacity: 0.8,
-          size: 40,
-        },
-        push: {
-          quantity: 4,
-        },
-        repulse: {
-          distance: 200,
-          duration: 0.4,
-        },
-        explode: {
-          quantity: 10,
-        },
-      },
-    },
-    particles: {
-      color: {
-        value: "#f1f1f1",
-      },
-      links: {
-        color: "#f1f1f1",
-        distance: 150,
-        enable: true,
-        opacity: 0.5,
-        width: 1,
-      },
-      collisions: {
-        enable: true,
-      },
-      move: {
-        direction: 'none',
-        enable: true,
-        outMode: "bounce",
-        random: false,
-        speed: 2,
-        straight: false,
-      },
-      number: {
-        density: {
-          enable: true,
-          value_area: 800,
-        },
-        value: 50,
-      },
-      opacity: {
-        value: 0.5,
-      },
-      shape: {
-        type: "square",
-      },
-      size: {
-        random: true,
-        value: 5,
-      },
-    },
-  };
+  const options: any = use(import('./options.json'));
 
   return (
     <Particles
       className="animate-fade-bg"
       id="tsparticles"
       init={particlesInit}
-      loaded={particlesLoaded}
       options={options}
     />
   );
